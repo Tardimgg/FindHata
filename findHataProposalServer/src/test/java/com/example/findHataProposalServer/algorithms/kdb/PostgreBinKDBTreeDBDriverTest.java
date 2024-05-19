bcc92c1254a38045b2364d4255feb0a0ee7972ee
Tardimgg <dmitry.taran333@mail.ru>
2024-05-19 23:49:28 +0300

    fixed a build error in the docker env

--- findHataProposalServer/src/test/java/com/example/findHataProposalServer/algorithms/kdb/PostgreBinKDBTreeDBDriverTest.java
+++ findHataProposalServer/src/test/java/com/example/findHataProposalServer/algorithms/kdb/PostgreBinKDBTreeDBDriverTest.java
@@ -1,6 +1,5 @@
 package com.example.findHataProposalServer.algorithms.kdb;
 
-import org.bouncycastle.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,8 +12,8 @@ class PostgreBinKDBTreeDBDriverTest {
 
 
         long[] arr = new long[]{110, 65, 23, 474576, 3241234, 1234, 67567};
-        byte[] bArr = driver.toByteArray(arr.clone());
-        long[] res = driver.toLongArray(bArr);
+        byte[] bArr = ByteTransform.toByteArray(arr.clone());
+        long[] res = ByteTransform.toLongArray(bArr);
 
         assertArrayEquals(arr, res);
     }
@@ -24,8 +23,8 @@ class PostgreBinKDBTreeDBDriverTest {
         PostgreBinKDBTreeDBDriver driver = new PostgreBinKDBTreeDBDriver(null);
 
         double[] arr = new double[]{12.124, 1425.235, 23.346, 53.234, 457.34536, 36364.34536, 4576457.6796899};
-        byte[] bArr = driver.toByteArray(arr.clone());
-        double[] res = driver.toDoubleArray(bArr);
+        byte[] bArr = ByteTransform.toByteArray(arr.clone());
+        double[] res = ByteTransform.toDoubleArray(bArr);
 
         assertArrayEquals(arr, res);
     }
